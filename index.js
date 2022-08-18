b0fa5baa78047a4f01f89bfccef173976c1bd024
Timothy Kiyeng <timothy.kiyeng@student.moringaschool.com>
2022-08-18 20:43:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names) {
+  const appreciation = [];
+  for (let i = 0; i < names.length; i++) {
+    const thankYou = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+    appreciation.push(thankYou);
+  }
+  return appreciation;
+}
+
+//writeCards(surprise);
+
+writeCards(names, "surprise");
+
+const interger = 10;
+function countDown(interger) {
+  let i = 10;
+  while (i >= 0) {
+    console.log(i--);
+  }
+  return interger;
+}
+countDown(interger);
